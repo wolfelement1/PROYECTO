13d4ac7ca499f67040b63a167da3aed375528f2d
JoseCondori17 <jose.condori.p@utec.edu.pe>
2024-03-13 22:47:05 -0500

    Se extendio una tipografia 'DM Sans'

--- system-sales/tailwind.config.js
+++ system-sales/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dmsans: ['"DM Sans"']
+      }
+    },
   },
   plugins: [],
 }
